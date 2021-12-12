cdf1f4e86e977f66ae3eb9734cdacc673c79cc1a
Gabriel Rayzal <gabriel.rayzal@gmail.com>
2021-12-12 19:00:02 +0100

    add compatibility for BP v4

--- src/main/java/fr/Alphart/BAT/Utils/BPInterfaceFactory.java
+++ src/main/java/fr/Alphart/BAT/Utils/BPInterfaceFactory.java
@@ -1,10 +1,12 @@
 package fr.Alphart.BAT.Utils;
 
-import java.util.Collection;
-
 import lombok.RequiredArgsConstructor;
+import net.alpenblock.bungeeperms.BPPermission;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 /**
  * Provide a clean way to support both version of BungeePerms
@@ -12,38 +14,25 @@ import net.md_5.bungee.api.plugin.Plugin;
 public class BPInterfaceFactory{
     
     public static PermissionProvider getBPInterface(final Plugin bpPlugin){
-        if(bpPlugin instanceof net.alpenblock.bungeeperms.BungeePerms){
-            return new BungeePermsV2((net.alpenblock.bungeeperms.BungeePerms) bpPlugin);
-        }else if(bpPlugin instanceof net.alpenblock.bungeeperms.platform.bungee.BungeePlugin){
-            return new BungeePermsV3((net.alpenblock.bungeeperms.platform.bungee.BungeePlugin) bpPlugin);
-        }
-        throw new RuntimeException("BungeePerms version not supported !");
+        return new BungeePermsV4((net.alpenblock.bungeeperms.platform.bungee.BungeePlugin) bpPlugin);
     }
     
     public interface PermissionProvider{
         public Collection<String> getPermissions(final CommandSender sender);
     }
-    
+
     @RequiredArgsConstructor
-    static class BungeePermsV3 implements PermissionProvider{
+    static class BungeePermsV4 implements PermissionProvider{
         private final net.alpenblock.bungeeperms.platform.bungee.BungeePlugin bpPlugin;
-        
+
         @Override
-        public Collection<String> getPermissions(CommandSender sender) {
-            return bpPlugin.getBungeeperms().getPermissionsManager().getUser(sender.getName()).getEffectivePerms();
+        public Collection<String> getPermissions(CommandSender sender)
+        {
+            return bpPlugin.getBungeeperms().getPermissionsManager().getUser(sender.getName())
+                           .getEffectivePerms(null, null)
+                           .stream()
+                           .map(BPPermission::getPermission)
+                           .collect(Collectors.toList());
         }
-        
     }
-    
-    @RequiredArgsConstructor
-    static class BungeePermsV2 implements PermissionProvider{
-        private final net.alpenblock.bungeeperms.BungeePerms bpPlugin;
-        
-        @Override
-        public Collection<String> getPermissions(CommandSender sender) {
-            return bpPlugin.getPermissionsManager().getUser(sender.getName()).getEffectivePerms();
-        }
-        
-    }
-    
 }
